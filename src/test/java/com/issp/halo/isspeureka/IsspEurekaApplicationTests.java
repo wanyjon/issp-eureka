5daa1d8ff49b6b4da1b05f89835120daf83e0484
wanyjon <779859662@qq.com>
2018-04-10 10:36:48 +0800

    first commit

---
+++ src/test/java/com/issp/halo/isspeureka/IsspEurekaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.issp.halo.isspeureka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IsspEurekaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
